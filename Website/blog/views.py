8f0f671afcb13cf07112c87722b1a05d50b03b83
Avison <avisonlivingstone@gmail.com>
2020-08-25 12:45:09 +0200

    'Database'

--- Website/blog/views.py
+++ Website/blog/views.py
@@ -1,30 +1,32 @@
 from django.shortcuts import render
+from .models import post
 
-db = [
-{
-'author':'Ayobami Abraham',
-'date':'April 2, 2020',
-'content':'Wecome to my first post. i will be giving you heavenly minded contents',
-'title':'My First Post'
-},
-{
-'author':'Tumi Adepoju',
-'date':'April 9, 2020',
-'content':'Wecome to my first post. my name is Tumi, a co founder of this blog',
-'title':'My Welcome message'
-},
-{
-'author':'Ayobami Abraham',
-'date':'April 14, 2020',
-'content':'Wecome to my Second post. Hope you were all blessed with my first post',
-'title':'My Second Post'
-}
+# !-- database dummy data
+# db = [
+# {
+# 'author':'Ayobami Abraham',
+# 'date':'April 2, 2020',
+# 'content':'Wecome to my first post. i will be giving you heavenly minded contents',
+# 'title':'My First Post'
+# },
+# {
+# 'author':'Tumi Adepoju',
+# 'date':'April 9, 2020',
+# 'content':'Wecome to my first post. my name is Tumi, a co founder of this blog',
+# 'title':'My Welcome message'
+# },
+# {
+# 'author':'Ayobami Abraham',
+# 'date':'April 14, 2020',
+# 'content':'Wecome to my Second post. Hope you were all blessed with my first post',
+# 'title':'My Second Post'
+# }
 
-]
+# ]
 
 
 def home(request):
-	content = {'database': db, 'tag':'Welcome!'}
+	content = {'database': post.objects.all(), 'tag':'Welcome!'}
 	return render(request,'blog/home.html',content)
 
 def about(request):
